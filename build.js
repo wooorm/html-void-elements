f29186df345e21806dac508082e130dbbb12be5c
Titus Wormer <tituswormer@gmail.com>
2016-11-07 19:50:03 +0100

    Replace cheerio with jsdom

--- build.js
+++ build.js
@@ -1,58 +1,24 @@
 'use strict';
 
-/* Dependencies. */
 var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var cheerio = require('cheerio');
+var jsdom = require('jsdom');
 var bail = require('bail');
 var list = require('./');
 
-/* Constants. */
-var HTTPS = 'https://';
+jsdom.env('http://w3c.github.io/html/syntax.html#void-elements', function (err, window) {
+  bail(err);
 
-/* Input / output locations. */
-var w3c = 'http://w3c.github.io/html/syntax.html#void-elements';
-var output = 'index.json';
+  var dfn = window.document.getElementById('void-elements');
+  var dd = dfn.parentNode.nextElementSibling;
+  var rows = [].slice.call(dd.querySelectorAll('code a'));
 
-/* Load. */
-function load(url, callback) {
-  var lib = url.slice(0, HTTPS.length) === HTTPS ? https : http;
+  rows.forEach(function (row) {
+    var data = row.textContent;
 
-  lib.get(url, function (res, err) {
-    var value = '';
-
-    if (err) {
-      return callback(err);
+    if (data && !/\s/.test(data) && list.indexOf(data) === -1) {
+      list.push(data);
     }
-
-    res
-      .setEncoding('utf8')
-      .on('data', function (buf) {
-        value += buf;
-      }).on('end', function () {
-        return callback(null, value);
-      });
   });
-}
 
-/* Crawl W3C. */
-load(w3c, function (err, doc) {
-  if (err) {
-    bail(err);
-  }
-
-  cheerio.load(doc)('#void-elements')
-    .parent()
-    .next()
-    .find('code a')
-    .each(function () {
-      var data = this.children[0].data;
-
-      if (data && list.indexOf(data) === -1) {
-        list.push(data);
-      }
-    });
-
-  fs.writeFile(output, JSON.stringify(list.sort(), 0, 2) + '\n', bail);
+  fs.writeFile('index.json', JSON.stringify(list.sort(), 0, 2) + '\n', bail);
 });
