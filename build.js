1d6c5d109b614eef1098582a2684f463effd147a
Titus Wormer <tituswormer@gmail.com>
2022-11-16 13:33:12 +0100

    Refactor build script

--- build.js
+++ build.js
@@ -1,48 +1,41 @@
-import assert from 'node:assert'
-import fs from 'node:fs'
-import https from 'node:https'
-import concatStream from 'concat-stream'
-import {unified} from 'unified'
-import rehypeParse from 'rehype-parse'
+import assert from 'node:assert/strict'
+import fs from 'node:fs/promises'
+import fetch from 'node-fetch'
+import {fromHtml} from 'hast-util-from-html'
 import {select, selectAll} from 'hast-util-select'
 import {toString} from 'hast-util-to-string'
-import {bail} from 'bail'
 import {htmlVoidElements} from './index.js'
 
-const processor = unified().use(rehypeParse)
+const response = await fetch(
+  'https://html.spec.whatwg.org/multipage/syntax.html'
+)
+const text = await response.text()
+const tree = fromHtml(text)
 
-https.get('https://html.spec.whatwg.org/multipage/syntax.html', (response) => {
-  response
-    .pipe(
-      concatStream((buf) => {
-        const dd = select('#elements-2 ~ dl dd', processor.parse(buf))
-        assert(dd, 'expected a `dd`')
-        const nodes = selectAll('code', dd)
-        let index = -1
+const dd = select('#elements-2 ~ dl dd', tree)
+assert(dd, 'expected a `dd`')
+const nodes = selectAll('code', dd)
+let index = -1
 
-        while (++index < nodes.length) {
-          const value = toString(nodes[index])
+while (++index < nodes.length) {
+  const value = toString(nodes[index])
 
-          if (value && !/\s/.test(value) && !htmlVoidElements.includes(value)) {
-            htmlVoidElements.push(value)
-          }
-        }
+  if (value && !/\s/.test(value) && !htmlVoidElements.includes(value)) {
+    htmlVoidElements.push(value)
+  }
+}
 
-        fs.writeFile(
-          'index.js',
-          [
-            '/**',
-            ' * List of HTML void tag names.',
-            ' *',
-            ' * @type {Array<string>}',
-            ' */',
-            'export const htmlVoidElements = ' +
-              JSON.stringify(htmlVoidElements.sort(), null, 2),
-            ''
-          ].join('\n'),
-          bail
-        )
-      })
-    )
-    .on('error', bail)
-})
+const list = [...new Set(htmlVoidElements)].sort()
+
+await fs.writeFile(
+  'index.js',
+  [
+    '/**',
+    ' * List of HTML void tag names.',
+    ' *',
+    ' * @type {Array<string>}',
+    ' */',
+    'export const htmlVoidElements = ' + JSON.stringify(list, null, 2),
+    ''
+  ].join('\n')
+)
